5c9c6fd73faf49b8b6687096bb391dc6c4c0194d
JoanValiente <joan.valiente.lorite@gmail.com>
2017-03-30 20:02:06 +0200

    0.1.7
    -Added Welcome Screen (static).
    -Added Congrat Screen (static).
    -Camera fixed.

---
+++ Gunbird 0.1.7 Visual/SDL4_Handout/ModuleWelcome.cpp
@@ -0,0 +1,64 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModulePlayer.h"
+#include "ModuleSea.h"
+#include "ModuleWelcome.h"
+#include "ModuleMine.h"
+#include "ModuleInput.h"
+#include "ModuleFadeToBlack.h"
+
+
+ModuleWelcome::ModuleWelcome()
+{
+	// Background 
+	welcome.x = 0;
+	welcome.y = 0;
+	welcome.w = 224;
+	welcome.h = 320;
+
+
+}
+
+ModuleWelcome::~ModuleWelcome()
+{}
+
+// Load assets
+bool ModuleWelcome::Start()
+{
+	LOG("Loading background assets");
+	bool ret = true;
+	welcometexture = App->textures->Load("background_welcome.png");
+	return ret;
+}
+
+bool ModuleWelcome::CleanUp()
+{	
+	App->textures->Unload(welcometexture);
+	LOG("Unloading stage");
+	return true;
+}
+
+// Update: draw background
+update_status ModuleWelcome::Update()
+{
+
+	// Draw everything --------------------------------------
+
+
+	App->render->Blit(welcometexture, 0, -320 + SCREEN_HEIGHT, &welcome, 0.75f);
+
+
+	if (App->input->keyboard[SDL_SCANCODE_SPACE] && change) {
+
+		change = false;
+		App->fade->FadeToBlack(this, App->mine, 1);
+		change = true;
+
+	}
+
+
+
+	return UPDATE_CONTINUE;
+}
---
+++ Gunbird 0.1.7 Visual/SDL4_Handout/ModuleWelcome.h
@@ -0,0 +1,27 @@
+#ifndef __ModuleWelcome_H__
+#define __ModuleWelcome_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class ModuleWelcome : public Module
+{
+public:
+	ModuleWelcome();
+	~ModuleWelcome();
+
+	bool Start();
+	bool CleanUp();
+	update_status Update();
+
+public:
+	bool change = true;
+	SDL_Texture* welcometexture = nullptr;
+	SDL_Rect welcome;
+
+};
+
+#endif
