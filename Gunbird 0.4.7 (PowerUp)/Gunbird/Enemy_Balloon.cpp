c78af3544c3f160cb67eb6cf520294a2aed5e183
JoanValiente <joan.valiente.lorite@gmail.com>
2017-04-21 20:57:13 +0200

    Trying path

--- Gunbird 0.4.7 (PowerUp)/Gunbird/Enemy_Balloon.cpp
+++ Gunbird 0.4.7 (PowerUp)/Gunbird/Enemy_Balloon.cpp
@@ -18,7 +18,8 @@ Enemy_Balloon::Enemy_Balloon(int x, int y) : Enemy(x, y)
 	fly.speed = 0.2f;
 	animation = &fly;
 
-	movement.PushBack({ 0.0f,-1.0f }, 50, &fly);
+	movement.PushBack({ 0.0f,-1.0f }, 100, &fly);
+	movement.PushBack({ 1.0f,-1.0f }, 100, &fly);
 
 	collider = App->collision->AddCollider({ 0, 0, 42,53 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 
